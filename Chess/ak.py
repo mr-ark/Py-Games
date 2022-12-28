df68cd39c9066b113f8a61d62942eb9c6a267eea
Annuay Raj-k [Ark] <100431877+mr-ark@users.noreply.github.com>
2022-12-28 11:38:21 +0530

    Update ak.py

--- Chess/ak.py
+++ Chess/ak.py
@@ -1,9 +1,16 @@
+#supports Pyscripter & google python local host
+
+
+#if using py scripter module1:
+
 From python import chess
 board=chess.board()
 board.push_san("e4")
 board.push_san("e5")
 
 Board
+
+#Module2:
 
 import chess
 board=chess.board()
